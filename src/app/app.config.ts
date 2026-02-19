2887b9ca6e2f39df8f55f4003fccc24312b21eae
Melvin 3PG <melvin.fallas@3pillarglobal.com>
2026-02-19 09:46:32 -0600

    feat(routing): add hash location strategy

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideRouter(routes, withHashLocation()),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]

9d7fff6795eb1affc3a1206dadc0bac81a3bf9b1
Melvin 3PG <melvin.fallas@3pillarglobal.com>
2026-02-19 11:53:20 -0600

    refactor(app): extract welcome page to HomePage component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    loadComponent: () => import('./home-page/home-page').then((m) => m.HomePage),
+  },
+  {
     path: 'doc',
     loadComponent: () => import('./doc-page/doc-page').then((m) => m.DocPage),
   },
